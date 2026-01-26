14bed8f14f56d61bf324681e9366260ce04a5977
maciejjanowski <maciej.janowski7@hotmail.com>
2026-01-26 12:59:53 +0100

    Initial project setup

---
+++ src/test/java/de/bachelorarbeit/ticketsystem/TicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bachelorarbeit.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
